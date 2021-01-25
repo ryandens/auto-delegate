d1ca4a2ca82512b7ba41c656b1b13d1210a92a30
Ryan Dens <ryan.dens@contrastsecurity.com>
2021-01-25 08:10:17 -0500

    :heavy_plus_sign: add auto-service annotation processor

--- auto-delegate-processor/build.gradle.kts
+++ auto-delegate-processor/build.gradle.kts
@@ -6,3 +6,9 @@ java {
     sourceCompatibility = JavaVersion.VERSION_15
     targetCompatibility = JavaVersion.VERSION_15
 }
+
+dependencies {
+    val autoServiceVersion = "1.0-rc7"
+    compileOnly("com.google.auto.service", "auto-service-annotations", autoServiceVersion)
+    annotationProcessor("com.google.auto.service", "auto-service", autoServiceVersion)
+}
