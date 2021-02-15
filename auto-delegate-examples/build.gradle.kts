122a2adbea42ed675fa911cb0253ceea62fecdc9
Ryan Dens <ryan.dens@contrastsecurity.com>
2021-02-15 12:28:38 -0500

    :heavy_plus_sign: setup junit and mockito for testing example classes implementation

--- auto-delegate-examples/build.gradle.kts
+++ auto-delegate-examples/build.gradle.kts
@@ -10,4 +10,11 @@ java {
 dependencies {
     compileOnly(project(":auto-delegate-annotations"))
     annotationProcessor(project(":auto-delegate-processor", "shadow"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.mockito:mockito-core:3.7.7")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
