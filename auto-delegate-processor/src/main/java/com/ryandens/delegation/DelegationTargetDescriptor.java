529b2a487c5a1481aee7adc622b16dc1498c03d8
Ryan Dens <ryan.dens@contrastsecurity.com>
2021-03-07 17:46:42 -0500

    :bug: add equals and hashcode to prevent future bugs, possibly

--- auto-delegate-processor/src/main/java/com/ryandens/delegation/DelegationTargetDescriptor.java
+++ auto-delegate-processor/src/main/java/com/ryandens/delegation/DelegationTargetDescriptor.java
@@ -1,5 +1,6 @@
 package com.ryandens.delegation;
 
+import java.util.Objects;
 import javax.lang.model.type.DeclaredType;
 
 /** Immutable value class encapsulating all the information about an inner type to delegate to */
@@ -9,8 +10,8 @@ final class DelegationTargetDescriptor {
   private final String fieldName;
 
   DelegationTargetDescriptor(final DeclaredType declaredType, final String fieldName) {
-    this.declaredType = declaredType;
-    this.fieldName = fieldName;
+    this.declaredType = Objects.requireNonNull(declaredType);
+    this.fieldName = Objects.requireNonNull(fieldName);
   }
 
   DeclaredType declaredType() {
@@ -19,5 +20,29 @@ final class DelegationTargetDescriptor {
 
   String fieldName() {
     return fieldName;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final DelegationTargetDescriptor that = (DelegationTargetDescriptor) o;
+    return declaredType.equals(that.declaredType) && fieldName.equals(that.fieldName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(declaredType, fieldName);
+  }
+
+  @Override
+  public String toString() {
+    return "DelegationTargetDescriptor{"
+        + "declaredType="
+        + declaredType
+        + ", fieldName='"
+        + fieldName
+        + '\''
+        + '}';
   }
 }
