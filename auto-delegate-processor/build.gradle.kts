4b293f67ffe124175cbf70d6c4cac804d34adfb2
Ryan Dens <ryandens735@gmail.com>
2021-10-03 18:59:45 -0400

    :arrow_up: upgrade dependencies

--- auto-delegate-processor/build.gradle.kts
+++ auto-delegate-processor/build.gradle.kts
@@ -25,9 +25,9 @@ tasks {
 
 dependencies {
     implementation("com.squareup", "javapoet", "1.13.0")
-    implementation("com.google.auto", "auto-common", "0.11")
+    implementation("com.google.auto", "auto-common", "1.1.2")
     implementation(project(":auto-delegate-annotations"))
-    val autoServiceVersion = "1.0-rc7"
+    val autoServiceVersion = "1.0"
     compileOnly("com.google.auto.service", "auto-service-annotations", autoServiceVersion)
     annotationProcessor("com.google.auto.service", "auto-service", autoServiceVersion)
 }
