881d601ee17744b7c60a9879f7fea15cf74bc596
Ryan Dens <ryandens735@gmail.com>
2024-01-09 14:26:42 -0800

    :bookmark: tag 0.3.1 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
         }
     }
     group = "com.ryandens"
-    version = "0.3.0"
+    version = "0.3.1"
 
     this.extensions.getByType<nebula.plugin.contacts.ContactsExtension>().run {
         addPerson(
